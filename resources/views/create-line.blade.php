db3a93ce9803c60d7e7d4f9870920697b4306ade
Marko Doornbos <doornbos.marko@gmail.com>
2019-05-20 12:13:31 +0200

    Implement create-line.blade.php

--- resources/views/create-line.blade.php
+++ resources/views/create-line.blade.php
@@ -5,7 +5,30 @@
 @endsection
 
 @section('content')
-  <div>
-      Content to go here.
+  <div class="content">
+    <form method="POST" action="/lines">
+      {{ csrf_field() }}
+
+      @if ($errors->any())
+        <div class="notification is-danger">
+          <ul>
+            @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+            @endforeach
+          </ul>
+        </div>
+      @endif
+
+      <div>
+        Product line name: <br>
+        <input type="text" name="name" placeholder="Product line name" required>
+      </div>
+
+      <br>
+
+      <div>
+        <button type="submit">Add Product Line</button>
+      </div>
+    </form>
   </div>
 @endsection
