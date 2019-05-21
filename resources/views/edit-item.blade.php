361de1a6ecc76c5f688e1bc8e45c9770de9199b9
Marko Doornbos <doornbos.marko@gmail.com>
2019-05-21 08:26:44 +0200

    Implement edit-item.blade.php

--- resources/views/edit-item.blade.php
+++ resources/views/edit-item.blade.php
@@ -1,11 +1,42 @@
 @extends('layouts.app')
 
 @section('title')
-  <title>Edit item</title>
+  <title>Edit {{$item->title}}</title>
 @endsection
 
 @section('content')
   <div>
-      Content to go here.
+    <h2>Edit {{$item->title}}</h2>
+    <form method="POST" action={{"/lines/$line->id/items/$item->id"}}>
+      @method('PATCH')
+      {{ csrf_field() }}
+
+      @if ($errors->any())
+        <div class="notification is-danger">
+          <ul>
+            @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+            @endforeach
+          </ul>
+        </div>
+      @endif
+
+      <div>
+        Item title: <br>
+        <input type="text" name="title" value="{{$item->title}}" required>
+      </div>
+      <div>
+        Ordered by: <br>
+        <input type="textarea" name="ordered_by" value="{{$item->ordered_by}}" required>
+        <br>
+        Enter as a comma-separated list enclosed with [].
+      </div>
+
+      <br>
+
+      <div>
+        <button type="submit">Edit {{$item->title}}</button>
+      </div>
+    </form>
   </div>
 @endsection
